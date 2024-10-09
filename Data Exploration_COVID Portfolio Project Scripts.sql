b193c904c97ec7672933aca8a15cb65aa6176eee
Isagani S. Caca Jr. <105489391+IsaganiSolisCaca@users.noreply.github.com>
2024-10-09 19:00:08 +0800

    Update and rename COVID Portfolio Project Scripts.sql to Data Exploration_COVID Portfolio Project Scripts.sql

--- Data Exploration_COVID Portfolio Project Scripts.sql
+++ Data Exploration_COVID Portfolio Project Scripts.sql
@@ -1,6 +1,7 @@
---SELECT *
---FROM PortfolioProject..CovidVaccinations
---order by 3,4
+/*
+Covid Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types, and more basic or fundamentals
+*/
 
 SELECT *
 FROM PortfolioProject..CovidDeaths
